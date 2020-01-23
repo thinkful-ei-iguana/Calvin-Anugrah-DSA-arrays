3c262c91c06774395b275a332be1576e4ed936fd
Jekel8888 <52115322+Jekel8888@users.noreply.github.com>
2020-01-23 10:24:51 -0700

    memory is made, array is made, practice array utilizes array which utilizes memory

--- documentation-array.js
+++ documentation-array.js
@@ -63,16 +63,4 @@ class Array {
   }
 }
 
-function main() {
-  Array.SIZE_RATIO = 3;
-
-  // Create an instance of the Array class
-  let arr = new Array();
-
-  // Add an item to the array
-  arr.push(3);
-
-  console.log(arr);
-}
-
 Array.SIZE_RATIO = 3;
--- practice-array.js
+++ practice-array.js
@@ -1,9 +1,22 @@
-import memory from "./Memory";
+import Array from "./Array";
+// What is the length, capacity and memory address of your array?
+// Add the following in the main function and then print the array:
+//     ...
+//     arr.push(5);
+//     arr.push(15);
+//     arr.push(19);
+//     arr.push(45);
+//     arr.push(10);
+// What is the length, capacity and memory address of your array? Explain the result of your program after adding the new lines of code.
 
-class Array {
-  constructor() {
-    this.length = 0;
-    this._capacity = 0;
-    this.ptr = memory.allocate(this.length);
-  }
+function main() {
+  Array.SIZE_RATIO = 3;
+
+  // Create an instance of the Array class
+  let arr = new Array();
+
+  // Add an item to the array
+  arr.push(3);
+
+  console.log(arr);
 }
